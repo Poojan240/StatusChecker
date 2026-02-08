331d5843912519397eb36a444b64fc3bdd8ab821
poojan240 <poojanbahadurpradhan@gmail.com>
2026-02-08 12:15:21 -0500

    added legit check.

--- src/main/java/com/example/statuschecker/StatusChecker.java
+++ src/main/java/com/example/statuschecker/StatusChecker.java
@@ -1,6 +1,18 @@
 package com.example.statuschecker;
 
 public class StatusChecker {
+
+
+    private boolean isLegit;
+
+    public boolean isLegit() {
+        return isLegit;
+    }
+
+    public void setLegit(boolean legit) {
+        isLegit = legit;
+    }
+
     public String getStatus() {
         return "in_progress";
     }
